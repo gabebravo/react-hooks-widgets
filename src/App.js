1eec73c9d3cc6c35e3039f51593e704e2e636f88
Gabe Bravo <gabecbravo@gmail.com>
2019-03-05 21:09:49 -0500

    Added the click handler to perform a google search in the searchbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React, { useState } from "react";
 
 function App() {
   const [ state, setState ] = useState({ query: '' })
+
+  function searchQuery() {
+    window.open(`https://google.com/search?q=${state.query}`, '_blank')
+  }
+
+  function handleKeyPress(e){
+    if(e.key === 'Enter'){
+      searchQuery();
+    }
+  }
   
   return (
     <div className="App">
       <h1>Hello Gabe</h1>
       <div className="form">
-        <input value={state.query} onChange={(e) => setState({ ...state, query: e.target.value })} />
-        <button>Search</button>
+        <input value={state.query} onChange={(e) => setState({ ...state, query: e.target.value })} onKeyPress={handleKeyPress}/>
+        <button onClick={searchQuery}>Search</button>
       </div>
     </div>
   );
