8feba466d64dc1a1448bf1f1d2751706f16f5c0c
Gabe Bravo <gabecbravo@gmail.com>
2019-05-20 14:54:53 -0400

    Completed the gallery section. Added some logic to trigger a delay and image increment state update via some input fields on each change event. It will trigger useEffect and kick of a state change and re-rendering.

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,22 +1,40 @@
 import React, { useEffect, useState } from 'react'
 import PICTURES from '../data/pictures'
 
+const SECONDS = 1000;
+const MIN_DELAY = 1 * SECONDS
+const MIN_INC = 1
+
 function Gallery(){
   const [index, setIndex] = useState(0);
+  const [delay, setDelay] = useState(3 * SECONDS);
+  const [increment, setIncrement] = useState(1);
 
 // ONLY QUEUE UP ONCE
   useEffect( () => {
     const interval = setInterval( () => {
       setIndex( storedIndex => { // storedIndex = latest state value / not original defined version >> because original value only gets set on initital load
-        return (storedIndex + 1) % PICTURES.length
+        return (storedIndex + increment) % PICTURES.length
       })
-    }, 4000)
+    }, delay)
 
-    // CLEANUP FUNCTION - will run right after every re-render (in this case, only on unmount)
+    // CLEANUP FUNCTION - will run right after every re-render
     return () => {
       clearInterval(interval) // native JS function for interval
     }
-  }, [])
+  }, [delay, increment])
+
+// on the change event, sets delay >> triggers use effect to set interval >> also cleanup function runs
+  const updateDelay = ev => {
+    const delay = Number(ev.target.value) * SECONDS
+    setDelay(delay < MIN_DELAY ? MIN_DELAY : delay)
+  }
+
+// on the change event, sets incrment for the number of pics to skip
+  const updateincrement = ev => {
+    const increment = Number(ev.target.value)
+    setIncrement(increment < MIN_INC ? MIN_INC : increment)
+  }
 
   return (
     <>
@@ -26,6 +44,16 @@ function Gallery(){
           src={PICTURES[index].image}
           alt="gallery"
         />
+        <div className="multiform">
+          <div>
+            Gallery Transition Delay (seconds):
+            <input type='number' onChange={updateDelay} />
+          </div>
+          <div>
+            Gallery Increment:
+            <input type='number' onChange={updateincrement} />
+          </div>
+        </div>
       </div>
     </>
   )
